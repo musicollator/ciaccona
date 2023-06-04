dba957ad1b9b605fea9b47c081e3f4b3bdef0ef9
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2023-06-04 17:51:38 +0200

    removed packery, enters accordion

--- docs/artists-limited.js
+++ docs/artists-limited.js
@@ -1,5 +1,4 @@
 import ImagesLoaded from "https://cdn.jsdelivr.net/npm/imagesloaded@5.0.0/+esm"
-import packeryLayout from 'https://cdn.jsdelivr.net/npm/packery@2.1.2/+esm'
 import config from "/js/config.js?v=1.0.3-beta"
 import codec from "/js/structure.js?v=1.0.3-beta"
 import { createPlayerSingleton } from "/js/playerSingleton.js?v=1.0.3-beta"
@@ -27,6 +26,33 @@ const templateDivider = (instrumentKey, instrumentDisplayName) => `
     <div id="${instrumentKey}" style="align-self: center; font-size:smaller;">&nbsp;</div>
 </div>`
 
+const templateAccordionItem = (id, head) => `
+<div class="accordion-item" style="background-color: transparent; border: none !important; border-width: 0;">
+    <h2 class="accordion-header" id="panelsStayOpen-headingOne-${id}" style="background-color: transparent; border: none !important; border-width: 0;">
+        <button class="accordion-button divider" 
+                style="border: none !important; border-width: 0;"
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#panelsStayOpen-collapseOne-${id}" 
+                aria-expanded="true" 
+                aria-controls="panelsStayOpen-collapseOne-${id}">
+        <div class="d-flex justify-content-between" style="width:100%;">
+            <div class="icon-base icon-${iconMap[id]}" style="align-self: center; border-radius: 2rem; float:right; background-color: #ffffff20;"></div>
+            <div style="align-self: center; font-size:smaller; margin-left: 0.33rem;">${head}</div>
+            <div id="${id}" style="align-self: center; font-size:smaller; margin-left: 0.33rem;">&nbsp;</div>
+        </div>
+        </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne-${id}" 
+         class="accordion-collapse collapse show" 
+         style="border: none !important; border-width: 0;"
+         aria-labelledby="panelsStayOpen-headingOne-${id}">
+        <div class="accordion-body" 
+             style="padding:0; border: none !important; border-width: 0;">
+        </div>
+    </div>
+</div>`
+
 const template = (data) => `
 <div class="list-item">
     <div id="li-artist${data.v}" 
@@ -36,7 +62,7 @@ const template = (data) => `
         data-v="${data.v}" 
         data-i="${data.bg}"
         title="${data.n}"
-        style="background-image: ${data.bg}; overflow:hidden;">
+        style="background-image: ${data.bg}; overflow:hidden; margin: .333rem 0;">
         <div class="d-flex flex-column justify-content-start" style="height:100%;" >
             <div class="hero-intro flex-shrink-1 align-self-start"
                 style="padding-right: 0.5rem;">
@@ -107,19 +133,23 @@ loadArtists().then(putainDeArtists => {
 
         const instrumentsCount = new Map()
         let previousInstrument = "_"
+        let accordeonItem 
+        let accordeonBody 
         data.forEach(d => {
             instrumentsCount.set(d.instrument, (instrumentsCount.get(d.instrument) || 0) + 1)
             if (d.instrument !== previousInstrument) {
                 const instrumentDisplayName = d.instrument.replaceAll(/\d/gi, '').replaceAll(/_/gi, '&#xA0;' /* No-Break Space */)
 
-                list.appendChild(generateElement(templateDivider(d.instrument, instrumentDisplayName)))
+                accordeonItem = generateElement(templateAccordionItem(d.instrument, instrumentDisplayName))
+                accordeonBody = accordeonItem.querySelector('.accordion-body')
+                list.appendChild(accordeonItem)
 
                 previousInstrument = d.instrument
             }
-            list.appendChild(generateElement(template(d)))
+            accordeonBody.appendChild(generateElement(template(d)))
         })
         for (let [key, value] of instrumentsCount) {
-            document.getElementById(key).innerHTML = `(${value})`
+            document.getElementById(key).innerHTML = `${value}`
         }
 
         var listArtistElements = document.querySelectorAll('.list-artist')
@@ -131,96 +161,76 @@ loadArtists().then(putainDeArtists => {
             image.element.style.visibility = 'inherit'
         });
 
-        {
-            console.log("about to create packery ...")
-            const thePackery = new packeryLayout(list, {
-                itemSelector: "#list .list-item",
-                gutter: 10,
-                initLayout: false,
-                resize: true,
-                percentPosition: false
-            })
+        function setEventListeners() {
+            // https://getbootstrap.com/docs/5.2/components/offcanvas/#via-javascript
+            const offcanvasElement = document.getElementById('theOffcanvas')
+            if (typeof config.offcanvasElementBootstrapped === 'undefined') config.offcanvasElementBootstrapped = new bootstrap.Offcanvas(offcanvasElement)
 
-            thePackery.on('layoutComplete', function () {
-                console.log("Packery layout complete");
-            })
+            document.querySelectorAll('.list-artist').forEach(element => {
+                element.addEventListener('click', (event) => {
+                    event.stopPropagation()
+                    event.preventDefault()
 
-            thePackery.layout()
-
-            function setEventListeners() {
-                // https://getbootstrap.com/docs/5.2/components/offcanvas/#via-javascript
-                /*
-                const offcanvasElementList = document.querySelectorAll('.offcanvas')
-                const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
-                */
-                const offcanvasElement = document.getElementById('theOffcanvas')
-                if (typeof config.offcanvasElementBootstrapped === 'undefined') config.offcanvasElementBootstrapped = new bootstrap.Offcanvas(offcanvasElement)
-
-                document.querySelectorAll('.list-artist').forEach(element => {
-                    element.addEventListener('click', (event) => {
-                        event.stopPropagation()
-                        event.preventDefault()
-
-                        if (coerce.color?.candidate) {
-                            coerce.variation = coerce.color?.candidate
-                        }
-
-                        config.offcanvasElementBootstrapped.hide()
-
-                        let artistObject = putainDeArtists.getArtistFromFullnameNospaceLowercaseNodiacritics(event.currentTarget.dataset.a)
-
-                        createPlayerSingleton(artistObject).then(result => {
-                            const artistAndTimings = result.value
-                            coerce.fullnameNospaceLowercaseNodiacritics = artistAndTimings.fullnameNospaceLowercaseNodiacritics
-
-                            // https://developer.mozilla.org/en-US/docs/Web/API/History/pushState and https://stackoverflow.com/a/3354511/1070215 
-                            const url = new URL(location);
-                            url.searchParams.set("a", artistAndTimings.fullnameNospaceLowercaseNodiacritics);
-                            if (typeof coerce.variation !== 'undefined') {
-                                url.searchParams.set("v", coerce.variation);
-                            }
-                            history.pushState({}, "", url);
-                        })
-                    })
-                })
-
-                offcanvasElement.addEventListener('show.bs.offcanvas', (event) => {
-                    if (coerce.color) {
-                        offcanvasElement.classList.add(coerce.color.clazz)
-                        offcanvasElement.classList.add(coerce.color.tonality)
-                        document.querySelectorAll(`.list-artist[data-a="${coerce.fullnameNospaceLowercaseNodiacritics}"]`).forEach(la => {
-                            la.classList.add('thisIsTheOne')
-                        })
-                        if (typeof coerce.color.candidate !== 'undefined') {
-                            document.querySelectorAll('.list-artist').forEach(la => {
-                                la.style.backgroundImage = bg(la.dataset.a, coerce.color.candidate)
-                            })
-                            return
-                        }
+                    if (coerce.color?.candidate) {
+                        coerce.variation = coerce.color?.candidate
                     }
-                    document.querySelectorAll('.list-artist').forEach(la => {
-                        la.style.backgroundImage = la.dataset.i
-                    })
-                })
-                offcanvasElement.addEventListener('hidden.bs.offcanvas', (event) => {
-                    if (coerce.color) {
-                        offcanvasElement.classList.remove(coerce.color.clazz)
-                        offcanvasElement.classList.remove(coerce.color.tonality)
-                    }
-                    document.querySelectorAll(`.list-artist`).forEach(la => {
-                        la.classList.remove('thisIsTheOne')
-                    })
-                    coerce.color = undefined
-                })
 
-                document.querySelectorAll('#dismiss-offcanvas').forEach(element => element.addEventListener('click', (event) => {
                     config.offcanvasElementBootstrapped.hide()
-                }))
-            }
 
-            setEventListeners()
+                    let artistObject = putainDeArtists.getArtistFromFullnameNospaceLowercaseNodiacritics(event.currentTarget.dataset.a)
 
+                    createPlayerSingleton(artistObject).then(result => {
+                        const artistAndTimings = result.value
+                        coerce.fullnameNospaceLowercaseNodiacritics = artistAndTimings.fullnameNospaceLowercaseNodiacritics
+
+                        // https://developer.mozilla.org/en-US/docs/Web/API/History/pushState and https://stackoverflow.com/a/3354511/1070215 
+                        const url = new URL(location);
+                        url.searchParams.set("a", artistAndTimings.fullnameNospaceLowercaseNodiacritics);
+                        if (typeof coerce.variation !== 'undefined') {
+                            url.searchParams.set("v", coerce.variation);
+                        }
+                        history.pushState({}, "", url);
+                    })
+                })
+            })
+
+            offcanvasElement.addEventListener('show.bs.offcanvas', (event) => {
+                if (coerce.color) {
+                    offcanvasElement.classList.add(coerce.color.clazz)
+                    offcanvasElement.classList.add(coerce.color.tonality)
+                    document.querySelectorAll(`.list-artist[data-a="${coerce.fullnameNospaceLowercaseNodiacritics}"]`).forEach(la => {
+                        la.classList.add('thisIsTheOne')
+                    })
+                    if (typeof coerce.color.candidate !== 'undefined') {
+                        document.querySelectorAll('.list-artist').forEach(la => {
+                            la.style.backgroundImage = bg(la.dataset.a, coerce.color.candidate)
+                        })
+                        return
+                    }
+                }
+                document.querySelectorAll('.list-artist').forEach(la => {
+                    la.style.backgroundImage = la.dataset.i
+                })
+            })
+
+            offcanvasElement.addEventListener('hidden.bs.offcanvas', (event) => {
+                if (coerce.color) {
+                    offcanvasElement.classList.remove(coerce.color.clazz)
+                    offcanvasElement.classList.remove(coerce.color.tonality)
+                }
+                document.querySelectorAll(`.list-artist`).forEach(la => {
+                    la.classList.remove('thisIsTheOne')
+                })
+                coerce.color = undefined
+            })
+
+            document.querySelectorAll('#dismiss-offcanvas').forEach(element => element.addEventListener('click', (event) => {
+                config.offcanvasElementBootstrapped.hide()
+            }))
         }
+
+        setEventListeners()
+
 
     })(document.getElementById('list'))
 })
